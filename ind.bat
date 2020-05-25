f8047350d85c4f6fa0448e76cc57f7e820ddf9f2
ritaagibalova <ritulyalyu@yandex.ru>
2020-05-25 22:42:59 +0300

    first commit

---
+++ ind.bat
@@ -0,0 +1,26 @@
+Echo off
+md agibalova
+cd agibalova
+md rita
+cd rita
+md viktorovna
+cd ..
+pause
+echo> 04012000.txt
+cd rita
+cd viktorovna
+echo> krasoto4ka.txt
+cd ..
+cd ..
+cd ..
+pause
+del agibalova /S/Q/F
+pause
+cd agibalova
+cd rita
+rd viktorovna
+cd ..
+rd rita
+cd ..
+rd agibalova
+pause
---
+++ test.bat
@@ -0,0 +1,9 @@
+echo off
+rem create new catalog
+md firstcat
+rem gfj
+cd firstcat
+md secondcat
+cd secondcat
+md thirdcat
+rem this need for pause of programm
